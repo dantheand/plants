c676272d6edfe18918b4d56f0adf8fc8d55d49a9
dantheand <danderson@bighatbio.com>
2023-11-30 20:18:45 -0800

    switching over to fastapi

--- backend/utils/dynamodb.py
+++ backend/utils/dynamodb.py
@@ -1,27 +1,27 @@
 import boto3
-
-
-# # Pull in secrets
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# aws_profile = os.getenv("AWS_PROFILE")
-# aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-# aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-#
-#
-# def _get_db_connection():
-#     dynamodb = boto3.resource(
-#         "dynamodb",
-#         aws_access_key_id=aws_access_key_id,
-#         aws_secret_access_key=aws_secret_access_key,
-#         region_name="us-west-2",
-#     )
-#     return dynamodb
+import os
 
 
 def _get_db_connection():
-    dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
+    # Check if running in AWS Lambda environment
+    if "AWS_EXECUTION_ENV" in os.environ:
+        # Running on AWS Lambda, use the execution role for credentials
+        dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
+    else:
+        # Running locally, load credentials from .env file
+        from dotenv import load_dotenv
+
+        load_dotenv()
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+        dynamodb = boto3.resource(
+            "dynamodb",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name="us-west-2",
+        )
+
     return dynamodb
 
 
--- scratch/scripts.sh
+++ scratch/scripts.sh
@@ -1,5 +1,5 @@
 # Package backend code into zip
-cd ~/project/plant-app/plants
+cd ~/project/plants
 zip -r9 ./backend.zip ./backend
 
 # Deploy lambda API function
@@ -19,3 +19,48 @@ aws lambda update-function-code \
 
 # Testing API integration
 curl https://2jbzjl9rj2.execute-api.us-west-2.amazonaws.com/test/plants
+
+# Package dependencies and lambda into zip
+cd ~/projects/plants/venv/lib/python3.9/site-packages
+zip -r9 ~/projects/plants/backend.zip .
+cd ~/projects/plants
+zip -g ./backend.zip -r backend
+
+# Deploy lambda API function
+aws lambda create-function \
+--profile plants \
+--zip-file fileb://backend.zip \
+--handler backend.main.handler \
+--runtime python3.9 \
+--function-name plant-app \
+--role arn:aws:iam::593627358790:role/lambda_db_reader
+
+
+
+# Install AWS compatible dependencies
+cd ~/projects/plants
+pip install \
+--platform manylinux2014_x86_64 \
+--target=backend_deps \
+--implementation cp \
+--python-version 3.9 \
+--only-binary=:all: --upgrade \
+-r backend/requirements.txt
+
+# Package lambda
+cd ~/projects/plants/backend_deps
+zip -r ~/projects/plants/backend.zip .
+cd ~/projects/plants
+zip -g ./backend.zip -r backend
+
+# Updating lambda function w/ FastAPI backend
+aws lambda update-function-code \
+--profile plants \
+--zip-file fileb://backend.zip \
+--function-name plant-app
+
+# Running FastAPI locally
+cd ~/projects/plants
+uvicorn backend.main:app --reload
+
+
