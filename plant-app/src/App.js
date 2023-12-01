47fb33ccc179105f32b0a1a6bd4922711af0175b
dantheand <danderson@bighatbio.com>
2023-12-01 13:19:00 -0800

    react frontend

---
+++ plant-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import PlantList from './PlantList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PlantList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
