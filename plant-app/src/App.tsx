42eaae37c022e391f706620f05e3562f0875a58f
dantheand <danderson@bighatbio.com>
2023-12-09 16:29:26 -0800

    auto redirect to login page

--- plant-app/src/App.tsx
+++ plant-app/src/App.tsx
@@ -2,11 +2,20 @@
 import React  from 'react';
 import {PlantList,PlantDetails} from './PlantComponents';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+    BrowserRouter as Router,
+    Routes,
+    Route, Navigate, Outlet,
 } from "react-router-dom";
 import {AuthenticatorComponent} from "./Authenticator";
+import {JWT_TOKEN_STORAGE} from "./constants";
+
+const ProtectedRoute = () => {
+    const token = localStorage.getItem(JWT_TOKEN_STORAGE);
+    if (!token) {
+        return <Navigate to="/login" replace={true} />;
+    }
+    return <Outlet />;
+}
 
 function App() {
       return (
@@ -14,8 +23,10 @@ function App() {
         <Routes>
             <Route path="/" element={<AuthenticatorComponent />} />
             <Route path="/login" element={<AuthenticatorComponent />} />
-            <Route path="/plants/" element={<PlantList />} />
-            <Route path="/plants/:plantId" element={<PlantDetails />} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/plants/" element={<PlantList />} />
+                <Route path="/plants/:plantId" element={<PlantDetails />} />
+            </Route>
         </Routes>
     </Router>
   );
