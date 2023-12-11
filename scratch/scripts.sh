3d05331a69d0eafa110e695d7f5e00617a0dfae1
dantheand <danderson@bighatbio.com>
2023-12-11 10:12:05 -0800

    cleanup

--- scratch/scripts.sh
+++ scratch/scripts.sh
@@ -1,30 +1,10 @@
-## Package backend code into zip
-#cd ~/project/plants
-#zip -r9 ./backend.zip ./backend
-#
-## Deploy lambda API function
-#aws lambda create-function \
-#--profile plants \
-#--zip-file fileb://backend.zip \
-#--handler backend.lambda_function.lambda_handler \
-#--runtime python3.9 \
-#--function-name plant-app \
-#--role arn:aws:iam::593627358790:role/lambda_db_reader
-#
-## Update lambda API function
-#aws lambda update-function-code \
-#--profile plants \
-#--zip-file fileb://backend.zip \
-#--function-name plant-app
-
 # Testing API integration
 curl https://2jbzjl9rj2.execute-api.us-west-2.amazonaws.com/test/plants
 
-## Package dependencies and lambda into zip
-#cd ~/projects/plants/venv/lib/python3.9/site-packages
-#zip -r9 ~/projects/plants/backend.zip .
-#cd ~/projects/plants
-#zip -g ./backend.zip -r backend
+
+####################################################################################################
+################################ Lambda function build and deploy ##################################
+####################################################################################################
 
 # Deploy original lambda API function
 aws lambda create-function \
@@ -35,10 +15,10 @@ aws lambda create-function \
 --function-name plant-app \
 --role arn:aws:iam::593627358790:role/lambda_db_reader
 
-
 # Install AWS compatible dependencies
 cd ~/projects/plants
 pip install \
+# This is needed to make sure we build the correct packages for the AWS lambda environment
 --platform manylinux2014_x86_64 \
 --target=api_deps \
 --implementation cp \
