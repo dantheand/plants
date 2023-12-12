9ed7e65c2fb15216ad762b7ff5f546a7ffce58bd
dantheand <danderson@bighatbio.com>
2023-12-11 22:49:32 -0800

    test

--- plant-app/src/constants.ts
+++ plant-app/src/constants.ts
@@ -1,5 +1,3 @@
-// TODO: check if in AWS and if so, switch to AWS API URL
-
 export const BASE_API_URL = process.env.REACT_APP_API_BASE_URL;
 export const JWT_TOKEN_STORAGE = "jwtToken";
 export const GOOGLE_CLIENT_ID =
