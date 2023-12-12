376585e3d66c2105615ec1cfc14e64cdf2febb1a
dantheand <danderson@bighatbio.com>
2023-12-11 22:56:11 -0800

    fixed

--- plant-app/src/constants.ts
+++ plant-app/src/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = process.env.REACT_APP_API_BASE_URL;
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 export const JWT_TOKEN_STORAGE = "jwtToken";
 export const GOOGLE_CLIENT_ID =
   "323044269310-jpacaee5fqigd05rolak62uto6mfnmcb.apps.googleusercontent.com";
