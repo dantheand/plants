d84a6b4f78fa0bf8141f7c213bdf5fd01c6f0860
dantheand <danderson@bighatbio.com>
2023-12-15 19:51:46 -0800

    adding new schema

--- api/utils/schema.py
+++ api/utils/schema.py
@@ -1,7 +1,68 @@
 from datetime import date, datetime
+from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, validator
+
+############################################
+######### New DynamoDB Models ##############
+############################################
+
+"""
+User:
+PK: User#<email>
+SK: User#<email>
+entity_type: User
+disabled: bool
+
+
+"""
+
+
+class EntityType(str, Enum):
+    USER = "User"
+    PLANT = "Plant"
+    IMAGE = "Image"
+    LINEAGE = "Lineage"
+
+
+class UserItem(BaseModel):
+    pk: str = Field(..., alias="PK", regex=r"^USER#")
+    sk: str = Field(..., alias="SK", regex=r"^USER#")
+    entity_type: str = Field(EntityType.USER)
+    disabled: bool
+
+
+class PlantItem(BaseModel):
+    pk: str = Field(..., alias="PK", regex=r"^USER#")
+    sk: str = Field(..., alias="SK", regex=r"^USER#")
+    entity_type: str = Field(EntityType.PLANT)
+    # TODO add the rest
+
+
+class ImageItem(BaseModel):
+    pk: str = Field(..., alias="PK", regex=r"^PLANT#")
+    sk: str = Field(..., alias="SK", regex=r"^IMAGE#")
+    entity_type: str = Field(EntityType.IMAGE)
+    timestamp: datetime
+    s3_url: str
+    signed_url: Optional[str] = None
+
+
+class PlantLineageItem(BaseModel):
+    pk: str = Field(
+        ...,
+        alias="PK",
+        regex=r"^PLANT#",
+        description="Partition key, must start with PLANT# and correspond to an offspring PlantItem's pk",
+    )
+    sk: str = Field(..., alias="SK", regex=r"^PARENT#", description="Sort key, must start with PARENT#")
+    entity_type: str = Field(EntityType.LINEAGE)
+
+
+############################################
+############### Original Models #############
+############################################
 
 
 class MyBaseModel(BaseModel):
