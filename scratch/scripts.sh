5f271c3f4bf6f288bbe1aa0e05dfc9ad7b2cb76e
dantheand <danderson@bighatbio.com>
2023-12-17 21:51:30 -0800

    trying to fix my things

--- scratch/scripts.sh
+++ scratch/scripts.sh
@@ -7,36 +7,37 @@ curl https://2jbzjl9rj2.execute-api.us-west-2.amazonaws.com/test/plants
 ####################################################################################################
 
 # Deploy original lambda API function
+cd ~/projects/plants/backend
 aws lambda create-function \
 --profile plants \
 --zip-file fileb://api_lambda.zip \
---handler api.main.handler \
+--handler backend.plant_api.main.handler \
 --runtime python3.9 \
---function-name plant-app \
+--function-name new-plant-app \
 --role arn:aws:iam::593627358790:role/lambda_db_reader
 
 # Install AWS compatible dependencies
-cd ~/projects/plants
+cd ~/projects/plants/backend/
 pip install \
 --platform manylinux2014_x86_64 \
---target=api_deps \
+--target=backend_deps \
 --implementation cp \
 --python-version 3.9 \
 --only-binary=:all: --upgrade \
 --index https://pypi.org/simple/ \
--r api/requirements.txt
+-r requirements.txt
 
 # Package lambda
-cd ~/projects/plants/api_deps
-zip -r ~/projects/plants/api_lambda.zip .
-cd ~/projects/plants
-zip -g ./api_lambda.zip -r api
+cd ~/projects/plants/backend/api_deps
+zip -r ~/projects/plants/backend/api_lambda.zip .
+cd ~/projects/plants/backend
+zip -g ./api_lambda.zip -r plant_api
 
 # Updating lambda function w/ FastAPI backend
 aws lambda update-function-code \
 --profile plants \
 --zip-file fileb://api_lambda.zip \
---function-name plant-app
+--function-name new-plant-app
 
 ####################################################################################################
 ################################## Local Development Environment ###################################
