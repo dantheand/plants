c30cffbde32977b436edd709d659afc9272cdbcb
dantheand <danderson@bighatbio.com>
2023-12-17 20:19:06 -0800

    try again

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -6,7 +6,7 @@ from backend.plant_api.utils.aws import get_aws_secret
 
 
 class TestAWSAccess:
-    @pytest.mark.skipif(os.environ["AWS_"], reason="requires python3.3")
+    @pytest.mark.skipif(os.environ.get("GITHUB_ENV") is not None, reason="This test doesn't work in GitHub builds.")
     def test_get_aws_secret(self):
         result = get_aws_secret("test_secret")
         assert result == "test_value"
