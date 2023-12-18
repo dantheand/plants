10d79265a17020393483f34e3caaef5e91047de7
dantheand <danderson@bighatbio.com>
2023-12-17 20:15:39 -0800

    trying out path:

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,7 +1,12 @@
+import os
+
+import pytest
+
 from backend.plant_api.utils.aws import get_aws_secret
 
 
 class TestAWSAccess:
+    @pytest.mark.skipif(os.environ["AWS_"], reason="requires python3.3")
     def test_get_aws_secret(self):
         result = get_aws_secret("test_secret")
         assert result == "test_value"
