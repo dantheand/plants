bdf52d14f67499a32bedc4d0f1793da40afeb716
dantheand <danderson@bighatbio.com>
2023-12-23 13:58:48 -0800

    added navbar w/ logo

--- plant-app/src/Layouts.tsx
+++ plant-app/src/Layouts.tsx
@@ -1,11 +1,35 @@
-import { Container } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 import { BackButton } from "./commonComponents";
 import React, { ReactNode } from "react";
 import { AlertComponent, AlertProvider } from "./AlertComponents";
+import plantBrandIcon from "./assets/plant_img_small.jpg";
+
+import "./styles.css";
+
+export const AppNavbar = () => {
+  return (
+    <Navbar expand="lg" className="custom-navbar">
+      <Container>
+        <Navbar.Brand href="/plants/">
+          <img
+            src={plantBrandIcon}
+            width="40" // Adjust size as needed
+            height="40"
+            className="d-inline-block align-top"
+            alt="Brand Icon"
+          />{" "}
+          Plant Tracker App
+        </Navbar.Brand>
+        {/* Other navbar content */}
+      </Container>
+    </Navbar>
+  );
+};
 
 export const GlobalLayout = ({ children }: { children: ReactNode }) => {
   return (
     <AlertProvider>
+      <AppNavbar />
       <AlertComponent />
       {children}
     </AlertProvider>
