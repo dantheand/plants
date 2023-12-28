17c723edf140bed94eebaad3ca4900b8bfab2df9
dantheand <danderson@bighatbio.com>
2023-12-28 10:51:32 -0800

    added camera icon

--- plant-app/src/components/Layouts.tsx
+++ plant-app/src/components/Layouts.tsx
@@ -1,11 +1,13 @@
 import { Container } from "react-bootstrap";
-import { BackButton } from "./CommonComponents";
+import { BackButton, FloatingActionButton } from "./CommonComponents";
 import React, { ReactNode } from "react";
 import { AlertComponent, AlertProvider } from "../context/Alerts";
 
 import "../styles/styles.css";
 import { useLocation } from "react-router-dom";
 import { AppNavbar } from "./AppNavbar";
+import { FaPlus } from "react-icons/fa";
+import { FaCamera } from "react-icons/fa6";
 
 export const GlobalLayout = ({ children }: { children: ReactNode }) => {
   // Exclude the navbar from the login page
@@ -28,6 +30,10 @@ export const PlantLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Container className="my-4">
       <BackButton /> {children}
+      <FloatingActionButton
+        icon={<FaCamera />}
+        handleOnClick={() => console.log("Clicked!")}
+      />
     </Container>
   );
 };
