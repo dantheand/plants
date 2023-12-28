fab95d0999211e510c9fd702e21d93b7919c3387
dantheand <danderson@bighatbio.com>
2023-12-28 10:48:46 -0800

    cleanup

--- plant-app/src/components/Layouts.tsx
+++ plant-app/src/components/Layouts.tsx
@@ -27,10 +27,7 @@ export const GlobalLayout = ({ children }: { children: ReactNode }) => {
 export const PlantLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Container className="my-4">
-      <BackButton />{" "}
-      {/*TODO: Either use context API to pass this info to DeleteButton, put the component in plantdetails,
-      or raise the state*/}
-      {children}
+      <BackButton /> {children}
     </Container>
   );
 };
