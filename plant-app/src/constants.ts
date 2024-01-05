b62c1d47ff89027e7347bacf8ca75e6a29861d31
dantheand <danderson@bighatbio.com>
2024-01-04 18:50:56 -0800

    how did i not realize the name was so close to polio

--- plant-app/src/constants.ts
+++ plant-app/src/constants.ts
@@ -5,4 +5,4 @@ export const JWT_TOKEN_STORAGE = "jwtToken";
 export const GOOGLE_CLIENT_ID =
   "323044269310-jpacaee5fqigd05rolak62uto6mfnmcb.apps.googleusercontent.com";
 
-export const APP_BRAND_NAME = "Folio";
+export const APP_BRAND_NAME = "Foliage";
