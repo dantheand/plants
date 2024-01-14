eaff513789cce6fd1f64e1ba47ab24f3a5eb8e6c
dantheand <danderson@bighatbio.com>
2024-01-14 12:41:33 -0800

    polling JWT expiration and redirecting

--- plant-app/src/App.tsx
+++ plant-app/src/App.tsx
@@ -14,6 +14,8 @@ import { PlantCreate } from "./views/PlantCreate";
 import { GlobalLayout } from "./components/Layouts";
 
 import "./styles/styles.css";
+import { jwtDecode } from "jwt-decode";
+import { JwtPayload } from "./types/interfaces";
 
 // TODO: improve this approach so that it doesn't require a full page refresh to send users to the login page
 //
@@ -22,7 +24,7 @@ const ProtectedRoute = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const checkTokenValidity = async () => {
+    const checkTokenWithBackend = async () => {
       const token = localStorage.getItem(JWT_TOKEN_STORAGE);
       if (!token) {
         setIsAuthenticated(false);
@@ -44,10 +46,11 @@ const ProtectedRoute = () => {
         }
 
         const data = await res.json();
+        setIsAuthenticated(data === true);
+
+        // If the backend verifies the token, set up client-side expiration check
         if (data === true) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
+          setUpTokenExpirationCheck(token);
         }
       } catch (error) {
         console.error("Error authenticating with backend:", error);
@@ -55,8 +58,38 @@ const ProtectedRoute = () => {
       }
       setIsLoading(false);
     };
-    checkTokenValidity();
+
+    const setUpTokenExpirationCheck = (token: string) => {
+      const checkTokenExpiration = () => {
+        try {
+          const decodedToken: JwtPayload = jwtDecode(token);
+          const currentUnixTimestamp = Date.now() / 1000;
+          if (!decodedToken || decodedToken.exp < currentUnixTimestamp) {
+            setIsAuthenticated(false);
+            return false;
+          }
+          return true;
+        } catch (error) {
+          console.error("Error decoding token:", error);
+          setIsAuthenticated(false);
+          return false;
+        }
+      };
+
+      // Set up an interval for continuous token validation
+      const intervalId = setInterval(() => {
+        if (!checkTokenExpiration()) {
+          clearInterval(intervalId);
+        }
+      }, 60000); // Check every minute
+
+      // Clean up interval on component unmount
+      return () => clearInterval(intervalId);
+    };
+
+    checkTokenWithBackend();
   }, []);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
