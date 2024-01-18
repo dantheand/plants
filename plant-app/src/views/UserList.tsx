2e5cd375b9b05450e89c2465f25fd1499d8200cc
dantheand <danderson@bighatbio.com>
2024-01-18 12:24:35 -0800

    showing current and total plants in frontend

--- plant-app/src/views/UserList.tsx
+++ plant-app/src/views/UserList.tsx
@@ -20,7 +20,8 @@ export const UserCard: React.FC<UserCardProps> = ({ user }) => {
       <Card>
         <Card.Body>
           <Card.Title>{user.email}</Card.Title>
-          <Card.Text>Number of Plants: {user.n_plants}</Card.Text>
+          <Card.Text>Current Number of Plant: {user.n_active_plants}</Card.Text>
+          <Card.Text>Total Plants Tracked: {user.n_total_plants}</Card.Text>
           <Button
             variant="primary"
             onClick={() => {
@@ -42,6 +43,9 @@ export const PlaceholderCard: React.FC = () => (
       <Card.Body>
         <Placeholder as={Card.Title} animation="glow">
           <Placeholder xs={7} />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation="glow">
+          <Placeholder xs={4} /> <Placeholder xs={1} />
         </Placeholder>
         <Placeholder as={Card.Text} animation="glow">
           <Placeholder xs={4} /> <Placeholder xs={1} />
@@ -71,7 +75,7 @@ export function UserList(): JSX.Element {
       })
       .then((data) => {
         const sortedUsers = data.sort((a: User, b: User) => {
-          return b.n_plants - a.n_plants;
+          return b.n_total_plants - a.n_total_plants;
         });
 
         setUsers(sortedUsers);
