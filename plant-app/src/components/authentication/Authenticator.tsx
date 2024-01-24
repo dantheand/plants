4b6376df3d0430e4b9395659172c807bcbb60b3a
dantheand <danderson@bighatbio.com>
2024-01-24 00:14:00 -0800

    adding create account button

--- plant-app/src/components/authentication/Authenticator.tsx
+++ plant-app/src/components/authentication/Authenticator.tsx
@@ -1,9 +1,9 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, Toast, ToastContainer } from "react-bootstrap";
 import { APP_BRAND_NAME, GOOGLE_CLIENT_ID } from "../../constants";
 import { useNavigate } from "react-router-dom";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import logo from "../../assets/plantopticon2_large_no_shadow.png";
 import cryptoRandomString from "crypto-random-string";
 import { useAuth } from "../../context/Auth";
@@ -20,10 +20,14 @@ export function AuthFromFrontEnd() {
 
   // And redirect to plants if user is already logged in
   const { login, isAuthenticated } = useAuth(); // Use the useAuth hook to get setIsAuthenticated
+  const [showCreateAccountToast, setShowCreateAccountToast] = useState(false); // State to control Toast visibility
 
   const handleGoogleSuccess = (response: CredentialResponse) => {
     login(response, nonce);
   };
+
+  const toggleShowToast = () =>
+    setShowCreateAccountToast(!showCreateAccountToast);
 
   const navigate = useNavigate();
   // Redirect if logged in
@@ -55,7 +59,34 @@ export function AuthFromFrontEnd() {
               />
             </div>
           </GoogleOAuthProvider>
+          <hr />
+          <Button
+            variant="secondary"
+            className={"create-account-button"}
+            onClick={toggleShowToast}
+          >
+            Create Account
+          </Button>
         </Card>
+        {/* Toast for displaying the message */}
+        <ToastContainer position={"bottom-center"} className={"mb-3"}>
+          <Toast
+            show={showCreateAccountToast}
+            onClose={toggleShowToast}
+            delay={5000}
+          >
+            <Toast.Header>
+              <strong className="me-auto">New Account Inforomation</strong>
+            </Toast.Header>
+            <Toast.Body>
+              <p>Interested in beta testing?</p>
+              <p>
+                Click the login button to transmit your account information and
+                then ask Dan to enable your account.
+              </p>
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
       </div>
     </>
   );
