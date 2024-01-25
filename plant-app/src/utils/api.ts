b5205ed1c9ae50fd9d5e79a5e1b170b4bac69ad0
dantheand <danderson@bighatbio.com>
2024-01-24 22:51:24 -0800

    linter

--- plant-app/src/utils/api.ts
+++ plant-app/src/utils/api.ts
@@ -1,11 +1,9 @@
 // TODO: standardize API calls into a function that takes in the endpoint, the method
 // and the body of the request, then catches different types of error (like API errors vs javascript errors) and
 
-import { useNavigate } from "react-router-dom";
 import { useCallback } from "react";
 
 export const useApi = () => {
-  const navigate = useNavigate();
   // useCallback is used to memoize the function so that it is not recreated on every render
   const callApi = useCallback(
     async (endpoint: string, options: RequestInit = {}) => {
@@ -18,6 +16,7 @@ export const useApi = () => {
         const response = await fetch(endpoint, fetchOptions);
         if (response.status === 401) {
           // Redirect to login page
+          // navigate("/login");
           // TODO: figure out why this infinite loops when the credentials error (happens when cookie is deleted)
           return Promise.reject(
             new Error("Unauthorized: please refresh and login again."),
@@ -32,7 +31,7 @@ export const useApi = () => {
         return Promise.reject(error);
       }
     },
-    [navigate],
+    [],
   );
   return { callApi };
 };
