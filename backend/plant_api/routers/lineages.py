b9401d1a1b1a55567d7760def45f558a0d27b802
dantheand <danderson@bighatbio.com>
2024-01-30 21:17:48 -0800

    lineage graph creator

---
+++ backend/plant_api/routers/lineages.py
@@ -0,0 +1,74 @@
+import logging
+from typing import Optional
+from uuid import UUID
+
+from plant_api.routers.common import BaseRouter
+from plant_api.dependencies import get_current_user_session
+from pydantic import BaseModel
+
+from fastapi import Depends
+
+from plant_api.routers.plants import read_all_plants_for_user
+
+logger = logging.getLogger(__name__)
+
+
+router = BaseRouter(
+    prefix="/lineages",
+    dependencies=[Depends(get_current_user_session)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class PlantLineageNode(BaseModel):
+    id: int  # this is the human_id
+    generation: Optional[int] = None
+    parents: Optional[list[int]] = None  # list of PlantLinearNode.id values
+
+
+class PlantLineageGraph(BaseModel):
+    levels: list[list[PlantLineageNode]]
+
+
+def assign_generations(plants: list[PlantLineageNode]) -> None:
+    plant_dict = {plant.id: plant for plant in plants}
+
+    def get_generation(plant_id: int) -> int:
+        plant = plant_dict[plant_id]
+        # If the generation is already assigned, return it
+        if plant.generation is not None:
+            return plant.generation
+        # Identify root plants
+        if plant.parents is None:
+            plant.generation = 0
+            return 0
+        # Plant's generation is one more than the maximum generation of its parents
+        plant.generation = max(get_generation(parent_id) for parent_id in plant.parents) + 1
+        return plant.generation
+
+    for plant in plants:
+        get_generation(plant.id)
+
+
+def assign_levels_to_generations(plants: list[PlantLineageNode]) -> list[list[PlantLineageNode]]:
+    # Throw value error if generation is not assigned
+    if any(plant.generation is None for plant in plants):
+        raise ValueError("Generation must be assigned to all plants before assigning levels.")
+
+    max_generation = max(plant.generation for plant in plants)
+    # Create placeholder list of lists
+    levels = [[] for _ in range(max_generation + 1)]
+    # Assign each plant to a level based on its generation
+    for plant in plants:
+        levels[plant.generation].append(plant)
+    return levels
+
+
+@router.get("/user/{user_id}", response_model=PlantLineageGraph)
+async def get_plant_lineage_graph(user_id: str):
+    # Construct a graph of plants based on their lineages
+    plants = read_all_plants_for_user(user_id)
+    plant_nodes = [PlantLineageNode(id=plant.human_id, parents=plant.parent_id) for plant in plants]
+    assign_generations(plant_nodes)
+    levels = assign_levels_to_generations(plant_nodes)
+    return PlantLineageGraph(levels=levels)
---
+++ backend/tests/test_lineages.py
@@ -0,0 +1,93 @@
+from plant_api.routers.lineages import (
+    PlantLineageGraph,
+    PlantLineageNode,
+    assign_generations,
+    assign_levels_to_generations,
+)
+from tests.lib import plant_record_factory
+
+from pydantic import TypeAdapter
+
+
+class TestGenerationAssignment:
+    def test_basic_generation_assignment(self):
+        # Create a set of related plants
+        root_plant = PlantLineageNode(id=0)
+        child_plant = PlantLineageNode(id=1, parents=[root_plant.id])
+        grandchild_plant = PlantLineageNode(id=2, parents=[child_plant.id])
+
+        assign_generations([root_plant, child_plant, grandchild_plant])
+        assert root_plant.generation == 0
+        assert child_plant.generation == 1
+        assert grandchild_plant.generation == 2
+
+    def test_generation_assignment_with_multiple_roots(self):
+        # Create a set of related plants
+        root_plant_1 = PlantLineageNode(id=0)
+        root_plant_2 = PlantLineageNode(id=1)
+        child_plant = PlantLineageNode(id=2, parents=[root_plant_1.id, root_plant_2.id])
+        grandchild_plant = PlantLineageNode(id=3, parents=[child_plant.id])
+
+        assign_generations([root_plant_1, root_plant_2, child_plant, grandchild_plant])
+        assert root_plant_1.generation == 0
+        assert root_plant_2.generation == 0
+        assert child_plant.generation == 1
+        assert grandchild_plant.generation == 2
+
+    def test_generation_assignment_with_multiple_roots_and_no_children(self):
+        # Create a set of related plants
+        root_plant_1 = PlantLineageNode(id=0)
+        root_plant_2 = PlantLineageNode(id=1)
+
+        assign_generations([root_plant_1, root_plant_2])
+        assert root_plant_1.generation == 0
+        assert root_plant_2.generation == 0
+
+    def test_generation_assignment_multiple_parents(self):
+        # Create a set of related plants
+        root_plant = PlantLineageNode(id=0)
+        child_plant = PlantLineageNode(id=1, parents=[root_plant.id])
+        grandchild_plant = PlantLineageNode(id=2, parents=[child_plant.id, root_plant.id])
+
+        assign_generations([root_plant, child_plant, grandchild_plant])
+        assert root_plant.generation == 0
+        assert child_plant.generation == 1
+        assert grandchild_plant.generation == 2
+
+
+class TestAssignLevels:
+    def test_assign_levels(self):
+        plant_1 = PlantLineageNode(id=0, parents=None, generation=0)
+        plant_2 = PlantLineageNode(id=1, parents=[0], generation=1)
+
+        result = assign_levels_to_generations([plant_1, plant_2])
+        assert len(result) == 2
+        assert len(result[0]) == 1
+        assert len(result[1]) == 1
+        assert result[0][0].id == 0
+        assert result[1][0].id == 1
+
+
+class TestPlantLineages:
+    def test_get_basic_lineage(self, mock_db, client_mock_session, default_enabled_user_in_db):
+        # Create a set of related plants
+        plant_1 = plant_record_factory(parent_id=None)
+        plant_2 = plant_record_factory(parent_id=[plant_1.human_id])
+        print(plant_1)
+        print(plant_2)
+
+        # Add the plants to the mock db
+        mock_db.insert_mock_data(plant_1)
+        mock_db.insert_mock_data(plant_2)
+
+        response = client_mock_session().get(f"/lineages/user/{default_enabled_user_in_db.google_id}")
+        assert response.status_code == 200
+        parsed_response = TypeAdapter(PlantLineageGraph).validate_python(response.json())
+        print(parsed_response)
+        assert len(parsed_response.levels) == 2
+        assert len(parsed_response.levels[0]) == 1
+        assert len(parsed_response.levels[1]) == 1
+        assert parsed_response.levels[0][0].id == plant_1.human_id
+        assert parsed_response.levels[1][0].id == plant_2.human_id
+        assert parsed_response.levels[0][0].parents is None
+        assert parsed_response.levels[1][0].parents == [plant_1.human_id]
