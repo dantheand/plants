5626dc2a34c6628a0d4bdbe5dd790bfcdf2fb9e7
dantheand <danderson@bighatbio.com>
2024-01-31 21:52:20 -0800

    lineages API endpoint and tests

--- backend/plant_api/routers/lineages.py
+++ backend/plant_api/routers/lineages.py
@@ -1,6 +1,6 @@
 import logging
 from collections import defaultdict
-from typing import Optional
+from typing import Optional, Sequence
 
 from plant_api.routers.common import BaseRouter
 from plant_api.dependencies import get_current_user_session
@@ -23,25 +23,29 @@ router = BaseRouter(
 
 class PlantLineageNode(BaseModel):
     id: int | str  # this is the human_id or a source/sink name
-    source: str = None
+    source: Optional[str] = None
     sink: Optional[str] = None
-    generation: Optional[int] = None
-    parents: Optional[list[int | str]] = None  # list of PlantLinearNode.id values
+    generation: int = -1  # -1 is placeholder generation value
+    parents: Optional[Sequence[int | str]] = None
 
 
 def create_nodes_for_sources(plants: list[PlantItem]) -> list[PlantLineageNode]:
+    """Creates a new node for each (non-plant)  source in the list of plants"""
     source_ids = []
     for plant in plants:
         # If the plant has no parent_id, then extract its source name to create a new node
         if plant.parent_id is None:
             source_ids.append(plant.source)
     # Dedeplicate sources and create a new plant node for each source
-    source_ids = set(source_ids)
+    source_ids = list(set(source_ids))
     return [PlantLineageNode(id=source_id) for source_id in source_ids]
 
 
 def create_nodes_for_sinks(plants: list[PlantItem]) -> list[PlantLineageNode]:
-    """Creates a new node for each sink in the list of plants and assigns parents as all plants who sunk into it"""
+    """Creates a new node for each sink in the list of plants
+
+    For each sink, it assigns all plants that sunk to it as parents
+    """
     sinks = defaultdict(list)
 
     for plant in plants:
@@ -50,49 +54,65 @@ def create_nodes_for_sinks(plants: list[PlantItem]) -> list[PlantLineageNode]:
     return [PlantLineageNode(id=sink, parents=sinks[sink]) for sink in sinks]
 
 
-def assign_generations_and_source_parents(plants: list[PlantLineageNode]) -> None:
-    plant_dict = {plant.id: plant for plant in plants}
+def create_nodes_for_plants(plants: list[PlantItem]) -> list[PlantLineageNode]:
+    """Creates a new node for each plant in the list of plants"""
+    return [PlantLineageNode(id=plant.human_id, source=plant.source, parents=plant.parent_id) for plant in plants]
 
-    def get_generation(plant_id: int) -> Optional[int]:
-        plant = plant_dict[plant_id]
+
+def assign_generations_and_source_parents(nodes: list[PlantLineageNode]) -> None:
+    """Creates generations from a list of node nodes.
+
+    Generations are grouping of plants based on the hierarchy of their parents.
+
+    We set the generation of source nodes to be 0 and set the generation of sink nodes to be one more than the maximum
+    generation of all plants.
+    """
+    node_dict = {node.id: node for node in nodes}
+
+    def get_generation(node_id: int | str) -> int:
+        node = node_dict[node_id]
         # If the generation is already assigned, return it
-        if plant.generation is not None:
-            return plant.generation
-        # Place sources at the root
-        if plant.source is None and plant.parents is None:
-            plant.generation = 0
+        if node.generation != -1:
+            return node.generation
+        # If the node has no source or parents, then it's a source and has generation 0
+        if node.source is None and node.parents is None:
+            node.generation = 0
             return 0
-        # If the plant has no parents, but has a source, then its a first generation plant
-        if plant.parents is None:
+        # If the node has no parents, but has a source, then its a first generation plant node
+        if node.parents is None and node.source is not None:
             # Also reassign its parent to be the source
-            plant.parents = [plant.source]
-            plant.generation = 1
+            node.parents = [node.source]
+            node.generation = 1
             return 1
-        # If the plant id is a string, then it's a sink and should wait to have its generation in a later step
-        if isinstance(plant.id, str):
-            return None
-        # Plant's generation is one more than the maximum generation of its parents
-        plant.generation = max(get_generation(parent_id) for parent_id in plant.parents) + 1
-        return plant.generation
+        # If the id is a string, then it's a sink node and we put a placeholder generation and assign it in a later step
+        if isinstance(node.id, str):
+            node.generation = -1
+            return -1
+        # Node's generation is one more than the maximum generation of its parents
+        if node.parents is not None:
+            node.generation = max(get_generation(parent_id) for parent_id in node.parents) + 1
+            return node.generation
+        return -1
 
-    for plant in plants:
-        get_generation(plant.id)
+    for node in nodes:
+        get_generation(node.id)
 
     # Assign sink generation to be one more than the maximum generation all plants
-    max_generation = max(plant.generation for plant in plants if plant.generation)
-    for plant in plants:
-        if plant.generation is None:
-            plant.generation = max_generation + 1
+    max_generation = max(node.generation for node in nodes if node.generation != -1)
+    for node in nodes:
+        if node.generation == -1:
+            node.generation = max_generation + 1
 
 
 def assign_levels_to_generations(plants: list[PlantLineageNode]) -> list[list[PlantLineageNode]]:
-    # Throw value error if generation is not assigned
-    if any(plant.generation is None for plant in plants):
-        raise ValueError("Generation must be assigned to all plants before assigning levels.")
+    """Groups plants into levels based on their generation"""
+    for plant in plants:
+        if plant.generation is None:
+            raise ValueError("Generation must be assigned to all plants before assigning levels.")
 
     max_generation = max(plant.generation for plant in plants)
     # Create placeholder list of lists
-    levels = [[] for _ in range(max_generation + 1)]
+    levels: list = [[] for _ in range(max_generation + 1)]
     # Assign each plant to a level based on its generation
     for plant in plants:
         levels[plant.generation].append(plant)
@@ -112,11 +132,10 @@ def assign_levels_to_generations(plants: list[PlantLineageNode]) -> list[list[Pl
 async def get_plant_lineage_graph(user_id: str):
     # Construct a graph of plants based on their lineages
     plants = read_all_plants_for_user(user_id)
-    plant_nodes = [
-        PlantLineageNode(id=plant.human_id, source=plant.source, parents=plant.parent_id) for plant in plants
-    ]
+    plant_nodes = create_nodes_for_plants(plants)
     source_nodes = create_nodes_for_sources(plants)
     sink_nodes = create_nodes_for_sinks(plants)
+
     all_plant_nodes = plant_nodes + source_nodes + sink_nodes
     assign_generations_and_source_parents(all_plant_nodes)
     levels = assign_levels_to_generations(all_plant_nodes)
--- backend/tests/test_lineages.py
+++ backend/tests/test_lineages.py
@@ -11,14 +11,16 @@ from pydantic import TypeAdapter
 class TestGenerationAssignment:
     def test_basic_generation_assignment(self):
         # Create a set of related plants
-        root_plant = PlantLineageNode(id=0)
+        source = PlantLineageNode(id="source")
+        root_plant = PlantLineageNode(id=0, parents=[source.id])
         child_plant = PlantLineageNode(id=1, parents=[root_plant.id])
         grandchild_plant = PlantLineageNode(id=2, parents=[child_plant.id])
 
-        assign_generations_and_source_parents([root_plant, child_plant, grandchild_plant])
-        assert root_plant.generation == 0
-        assert child_plant.generation == 1
-        assert grandchild_plant.generation == 2
+        assign_generations_and_source_parents([source, root_plant, child_plant, grandchild_plant])
+        assert source.generation == 0
+        assert root_plant.generation == 1
+        assert child_plant.generation == 2
+        assert grandchild_plant.generation == 3
 
     def test_generation_assignment_with_multiple_roots(self):
         # Create a set of related plants
@@ -35,12 +37,14 @@ class TestGenerationAssignment:
 
     def test_generation_assignment_with_multiple_roots_and_no_children(self):
         # Create a set of related plants
-        root_plant_1 = PlantLineageNode(id=0)
-        root_plant_2 = PlantLineageNode(id=1)
+        source = PlantLineageNode(id="source")
+        root_plant_1 = PlantLineageNode(id=0, parents=[source.id])
+        root_plant_2 = PlantLineageNode(id=1, parents=[source.id])
 
-        assign_generations_and_source_parents([root_plant_1, root_plant_2])
-        assert root_plant_1.generation == 0
-        assert root_plant_2.generation == 0
+        assign_generations_and_source_parents([source, root_plant_1, root_plant_2])
+        assert source.generation == 0
+        assert root_plant_1.generation == 1
+        assert root_plant_2.generation == 1
 
     def test_generation_assignment_multiple_parents(self):
         # Create a set of related plants
@@ -52,6 +56,17 @@ class TestGenerationAssignment:
         assert root_plant.generation == 0
         assert child_plant.generation == 1
         assert grandchild_plant.generation == 2
+
+    def test_generation_assignment_w_sink(self):
+        # Create a set of related plants
+        root_plant = PlantLineageNode(id=0)
+        child_plant = PlantLineageNode(id=1, parents=[root_plant.id])
+        sink = PlantLineageNode(id="sink", parents=[child_plant.id])
+
+        assign_generations_and_source_parents([root_plant, child_plant, sink])
+        assert root_plant.generation == 0
+        assert child_plant.generation == 1
+        assert sink.generation == 2
 
 
 class TestAssignLevels:
@@ -70,20 +85,23 @@ class TestAssignLevels:
 class TestPlantLineages:
     def test_get_basic_lineage(self, mock_db, client_mock_session, default_enabled_user_in_db):
         # Create a set of related plants
-        plant_1 = plant_record_factory(parent_id=None)
-        plant_2 = plant_record_factory(parent_id=[plant_1.human_id])
+        root_plant = plant_record_factory(parent_id=None, source="source", sink=None, sink_date=None)
+        child_plant = plant_record_factory(parent_id=[root_plant.human_id], sink="sink", sink_date="2021-01-01")
 
         # Add the plants to the mock db
-        mock_db.insert_mock_data(plant_1)
-        mock_db.insert_mock_data(plant_2)
+        mock_db.insert_mock_data(root_plant)
+        mock_db.insert_mock_data(child_plant)
 
         response = client_mock_session().get(f"/lineages/user/{default_enabled_user_in_db.google_id}")
         assert response.status_code == 200
         parsed_response = TypeAdapter(list[list[PlantLineageNode]]).validate_python(response.json())
-        assert len(parsed_response) == 2
-        assert len(parsed_response[0]) == 1
-        assert len(parsed_response[1]) == 1
-        assert parsed_response[0][0].id == plant_1.human_id
-        assert parsed_response[1][0].id == plant_2.human_id
+        print(parsed_response)
+        assert len(parsed_response) == 4
+        for level in parsed_response:
+            assert len(level) == 1
+        assert parsed_response[0][0].id == root_plant.source
+        assert parsed_response[1][0].id == root_plant.human_id
+        assert parsed_response[2][0].id == child_plant.human_id
         assert parsed_response[0][0].parents is None
-        assert parsed_response[1][0].parents == [plant_1.human_id]
+        assert parsed_response[1][0].parents == [root_plant.source]
+        assert parsed_response[2][0].parents == [root_plant.human_id]
